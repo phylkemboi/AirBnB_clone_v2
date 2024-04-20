43a9d2e4bde353db86e724a0e0d0a82e3f4c4a47
effgrandad <effgrandad@gmail.com>
2024-04-20 14:34:23 -0700

    AirBnB_clone_v2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>.
 """
-from flask import Flask
+Starts a Flask web application
+"""
 
+from flask import Flask
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_hbnb():
+def index():
     """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
@@ -25,11 +20,11 @@ def hbnb():
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def c(text):
+def cisfun(text):
     """Displays 'C' followed by the value of <text>."""
     text = text.replace("_", " ")
     return "C {}".format(text)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port="5000")
